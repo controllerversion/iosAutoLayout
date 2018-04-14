7cd7adf2efc22ad7bb859ddb25f89d4440759c84
StayFantasy <stayfantasy@gmail.com>
2018-04-14 21:05:36 +0800

    interface builder test

---
+++ autolayout_test/autolayout_test/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  autolayout_test
+//
+
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autolayout_test/autolayout_test/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  autolayout_test
+//
+
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
