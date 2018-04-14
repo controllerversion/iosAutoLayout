3db42544e7f2bb98544902003bdc45d930fa8c8c
StayFantasy <stayfantasy@gmail.com>
2018-04-14 21:09:06 +0800

    test_Push

---
+++ chap2_ios_service/chap2_ios_service/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  chap2_ios_service
+//
+
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chap2_ios_service/chap2_ios_service/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  chap2_ios_service
+//
+
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
